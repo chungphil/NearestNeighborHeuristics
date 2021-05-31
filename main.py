7f64c20652dc5d386ff83e10dd804c159dbad56e
Phillip <mrphillipchung@gmail.com>
2021-06-01 06:07:58 +1200

    NNH and Saving value completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #import a4.utility as utility
 #import a4.loader as loader
+import copy
+
 import numpy as np
 import loader
 import utility
@@ -11,43 +13,41 @@ import utility
 def main():
 
     # Paths to the data and solution files.
-    vrp_file = "n32-k5.vrp"  # "data/n80-k10.vrp"
-    sol_file = "n32-k5.sol"  # "data/n80-k10.sol"
+    vrp_file = "n80-k10.vrp"  # "data/n80-k10.vrp"
+    sol_file = "n80-k10.sol"  # "data/n80-k10.sol"
+
+    vrp_file80 = "n80-k10.vrp"
+    sol_file80 = "n80-k10.sol"
 
     # Loading the VRP data file.
     px, py, demand, capacity, depot = loader.load_data(vrp_file)
-    # px , py , demand(sum demand is 410) arrays of values, capacity is 100 for 5 vehicles, depot is at px[0] and py[0]
 
-    # print(px)
-    # print(py)
-    # print(demand)
-    # print(capacity)
-    # print(np.sum(demand))
-    # print(depot)
+
 
 
     # Displaying to console the distance and visualizing the optimal VRP solution.
-    # vrp_best_sol = loader.load_solution(sol_file)
-    # best_distance = utility.calculate_total_distance(vrp_best_sol, px, py, depot)
-    # print("Best VRP Distance:", best_distance)
-    # utility.visualise_solution(vrp_best_sol, px, py, depot, "Optimal Solution")
+    vrp_best_sol = loader.load_solution(sol_file)
+    best_distance = utility.calculate_total_distance(vrp_best_sol, px, py, depot)
+    print("Best VRP Distance:", best_distance)
+    utility.visualise_solution(vrp_best_sol, px, py, depot, "Optimal Solution")
 
     # Executing and visualizing the nearest neighbour VRP heuristic.
     # Uncomment it to do your assignment!
 
-    # nnh_solution = nearest_neighbour_heuristic(px, py, demand, capacity, depot)
-    # nnh_distance = utility.calculate_total_distance(nnh_solution, px, py, depot)
-    # print("Nearest Neighbour VRP Heuristic Distance:", nnh_distance)
-    # utility.visualise_solution(nnh_solution, px, py, depot, "Nearest Neighbour Heuristic")
+    nnh_solution = nearest_neighbour_heuristic(px, py, demand, capacity, depot)
+    nnh_distance = utility.calculate_total_distance(nnh_solution, px, py, depot)
+    print("Nearest Neighbour VRP Heuristic Distance:", nnh_distance)
+    utility.visualise_solution(nnh_solution, px, py, depot, "Nearest Neighbour Heuristic")
 
     # Executing and visualizing the saving VRP heuristic.
     # Uncomment it to do your assignment!
     
     sh_solution = savings_heuristic(px, py, demand, capacity, depot)
-    print(sh_solution)
     sh_distance = utility.calculate_total_distance(sh_solution, px, py, depot)
     print("Saving VRP Heuristic Distance:", sh_distance)
     utility.visualise_solution(sh_solution, px, py, depot, "Savings Heuristic")
+
+
 
 
 def nearest_neighbour_heuristic(px, py, demand, capacity, depot):
@@ -68,7 +68,7 @@ def nearest_neighbour_heuristic(px, py, demand, capacity, depot):
     routes = []
     eMat = utility.euclidean_matrix(px,py)
     visitedNodes = [depot]
-    demandLeft = demand
+    demandLeft = copy.deepcopy(demand)
     demandLeft[depot] = float('inf')
 
     while len(visitedNodes)< Len: # loop runs while nodes left to be visited
@@ -123,7 +123,6 @@ def savings_heuristic(px, py, demand, capacity, depot):
     """
     sMat = utility.savings_matrix(px,py, depot) # 32 x 32, depot not taken out.
     bestSave = np.unique(sMat) # list of best saving
-    visitedNodes = [depot]
 
     allroutes = [] #list of lists [[][]] with [depot,index numbers,depot].
     for i in range(len(px)):
